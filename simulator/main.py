4e2a068b9dc477b9b1f0348f7f08f8bb0b8098a1
djessy <94405549+Djessbourg@users.noreply.github.com>
2022-02-23 20:40:08 +0100

    supression des lignes inutiles de main

--- simulator/main.py
+++ simulator/main.py
@@ -2,13 +2,6 @@ import pygame as pg
 import motor as m
 
 if __name__ == '__main__' :
-
     pg.init()
-    # Démarrage du jeu
     motor = m.Motor()
     motor.run()
-    # Redémarrage du jeu en cas de redimension de la fenêtre
-    while motor.restart:
-        pg.init()
-        motor = m.Motor()
-        motor.run()
