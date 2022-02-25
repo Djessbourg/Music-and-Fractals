215154a67d2a3ec1323126d8d19dab940ef627aa
djessy <94405549+Djessbourg@users.noreply.github.com>
2022-02-25 13:20:54 +0100

    Mise à jour de fract.py
    -ajout des variables Julia et coord
    -ajout de fonctions récupératrices 
    -ajout d’une fonction is_in

--- simulator/fract.py
+++ simulator/fract.py
@@ -6,19 +6,21 @@ from typing import Callable # on en a besoin dans la dataclass pour insrire une 
 
 @dataclass
 class Fract:
-        size: list[int]                          # un couple sous la forme [500,500]
+        size: list[int]                          # un couple sous la forme [H,L]
         eq : Callable[[complex,complex],complex] # hollomorphisme à deux entrées
         color : str                              # couleurs de la carte d'affichage
         name : str                               # nom de la fractale
+        coord : list[int]                        # positions des extremums du plan sous la form [XMIN,XMAX,YMIN,YMAX]
+        julia : int                              # indique le type de fractale ( 0 = type Mandelbrot et  1 = type julia)
 
 class Fract_manager:
     def __init__(self) :
         self.currentfract = "Mandelbrot"
         self.fracts = dict()
-        self.frac_register([500,500],lambda z,c : z**2 +c ,"basic","Mandelbrot")
+        self.frac_register([500,500],lambda z,c : z**2 +c ,"basic","Mandelbrot",[-2, +0.5, -1.25, +1.25],0)
 
-    def frac_register(self, size, eq, color, name):
-        self.fracts[name] = Fract(size, eq, color, name)
+    def frac_register(self, size, eq, color, name, coord,julia):
+        self.fracts[name] = Fract(size, eq, color, name, coord,julia)
 
     def switch_fract(self,name):
         'change la fractale en cours'
@@ -26,11 +28,42 @@ class Fract_manager:
 
     def eval(self,c,n):
         'évalue un point du plan complexe de la fractale en cours'
-        eq = get_eq()
+        eq = self.get_eq()
         if n == 0 :
             return 0
         else :
             return eq(eval(c,n-1),c)
+
+    def is_in(self,x,y,MAX_ITERATION = 100, NORME = 4):
+        'détermine si un point de coordonées pygame x y est dans la partie convergente de la fractale'
+        Julia = self.get_julia
+        f = self.get_eq
+        C = self.get_coord()
+        S = self.get_size()
+        cx = C[0] + (x/S[0]) * (C[1] - C[0])
+        cy = -1*(C[2] +(y/S[1]) * (C[3] - C[2]))
+        if Julia == 0 :
+            C = complex (cx,cy)
+            Xn = complex(0,0)
+            n = 0
+            while abs(Xn)**2 < NORME and n < MAX_ITERATION :
+                Xn = f(Xn ,C)
+                n = n + 1
+            if n == MAX_ITERATION:
+                return 1
+            else :
+                return 0
+        if Julia == 1 :
+            C = complex (cx,cy)
+            Xn = complex (cx,cy)
+            n = 0
+            while abs(Xn)**2 < NORME and n < MAX_ITERATION :
+                Xn = f(Xn ,C)
+                n = n + 1
+            if n == MAX_ITERATION:
+                return 1
+            else :
+                return 0
 
     def get_delta(self,point, partie):
         'retourne la différence de partie réelle (real) ou imaginaire (imag)'
@@ -41,7 +74,7 @@ class Fract_manager:
         elif partie == 'imag':
             return(pl.imag(z2)-pl.imag(z1))
 
-#les fonctions get sont de type récupération de données
+#les fonctions get sont de type récupératrice de données
     def get_fract(self):
         'récupère la fractale courante'
         return self.fracts[self.currentfract]
@@ -61,3 +94,11 @@ class Fract_manager:
     def get_size(self):
         'renvoie la taille de la fenètre de la fractale en cours'
         return self.get_fract().size
+
+    def get_coord(self):
+        'renvoie les coordonées des extrèmes de la fenètre'
+        return self.get_fract().coord
+
+    def get_julia(self):
+        'renvoie l entier id de julia'
+        return self.get_fract().julia
